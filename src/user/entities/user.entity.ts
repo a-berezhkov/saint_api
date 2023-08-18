2d708780b6560e4e889d9f9c879fe1de248405f0
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-08-19 01:42:11 +0300

    [FEAT] add UserEntity for Swagger

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,35 @@
-export class User {}
+import { user } from "@prisma/client";
+import { ApiProperty } from "@nestjs/swagger";
+
+export class UserEntity implements user {
+
+  @ApiProperty({ required: false })
+  id: string;
+
+  @ApiProperty({ required: true })
+  accessToken: string;
+
+  @ApiProperty({ required: false })
+  created_at: Date;
+
+  @ApiProperty({ required: false })
+  email: string;
+
+  @ApiProperty({ required: true })
+  expires: Date;
+
+  @ApiProperty({ required: false })
+  github_id: string;
+
+  @ApiProperty({ required: false })
+  google_id: string;
+
+  @ApiProperty({ required: false })
+  image_github: string;
+
+  @ApiProperty({ required: false })
+  last_log_in: Date;
+
+  @ApiProperty({ required: true })
+  name: string;
+}
