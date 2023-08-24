8578ef3c55b6ad82f536d7c21df5a849a4231809
Anddrei V. Berreezhkov <berezhkov.mail@yandex.ru>
2023-08-24 23:53:11 +0300

    [FIX] update DTO and Prisma models

--- src/chapter/entities/chapter.entity.ts
+++ src/chapter/entities/chapter.entity.ts
@@ -1,19 +1,46 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { chapter } from "@prisma/client";
 
-export class ChapterEntity implements chapter{
-  @ApiProperty({ required: false })
-  id: string;
+export class ChapterEntity implements chapter {
+  @ApiProperty({
+    description: "ID in mongo DB",
+    required: false
+  })
+  id: string | null;
 
-  @ApiProperty({ required: false, nullable: true })
+  @ApiProperty({
+    description: "Description of chapter",
+    required: false,
+    type: String,
+    nullable: true,
+    example: "How work css in the web....."
+  })
   desc: string | null;
 
-  @ApiProperty({ required: true })
+  @ApiProperty({
+    description: "Name of chapter",
+    required: true,
+    type: String,
+    nullable: false,
+    example: "Css in the web"
+  })
   title: string;
 
-  @ApiProperty({ required: true })
+  @ApiProperty({
+    description: "Course ID",
+    required: true,
+    type: String,
+    nullable: true,
+    example: "64d63c73980ffe56f1c0c2fd"
+  })
   course_id: string;
 
-  @ApiProperty({ required: false })
-  order: number;
+  @ApiProperty({
+    description: "Order of chapters in one course. Autoincrement by course",
+    required: false,
+    type: Number,
+    nullable: true,
+    default: 0,
+  })
+  order: number | null;
 }
